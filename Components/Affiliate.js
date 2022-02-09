e4e57fee7dc98b472fbe2bba76913f143972a978
MOHDAHASANKHAN1 <95666818+MOHDAHASANKHAN1@users.noreply.github.com>
2022-02-09 23:36:37 +0530

    initial

--- Components/Affiliate.js
+++ Components/Affiliate.js
@@ -151,6 +151,274 @@ function Affiliate() {
                 <br />
                 <br />
             </div>
+
+            <div className="Affiliate-color2">
+                <br /><br /><br />
+                <div className="container">
+                    <p className='text-center h2'><b>When You Start, You Will Have A High Commission...</b></p>
+                    <br />
+                    <br />
+                    <div className="row g-0">
+                        <div className="col-sm-3">
+                            <img src="/Affiliateimg1.png" className="img-fluid " alt="/" />
+                        </div>
+                        <div className="col-sm-3">
+                            <img src="/Affiliateimg2.png" className="img-fluid " alt="/" />
+                        </div>
+                        <div className="col-sm-3">
+                            <img src="/Affiliateimg3.png" className="img-fluid " alt="/" />
+                        </div>
+                        <div className="col-sm-3">
+                            <img src="/Affiliateimg4.png" className="img-fluid " alt="/" />
+                        </div>
+                    </div>
+                    <br />
+                    <p className='text-center h2'><b>... And Soon You Will Have Even Higher Commission.</b></p>
+                    <br />
+                    <div className='d-flex justify-content-center'>
+                        <Link href="/Contact">
+                            <a className="px-4">
+                                <button type="button" className="btn btn-primary px-4 py-3 btn-lg text-white"><b>REGISTER AND START SELLING</b></button>
+                            </a>
+                        </Link>
+                    </div>
+                    <br />
+                    <p className='text-center h5 text-muted'>As a new Affiliate, you start from the BRONZE level with a 30% commission for every sale made with your affiliate link. Once your all-time sales reach $5 000, you will raise on the SILVER level and start getting a 35% commission. You will raise on the GOLD level and get 40% commission once your sales reach $15 000. After $50 000, you will be raise on the PLATINUM level.</p>
+                    <br /><br /><br />
+                    <div className="row g-0 p-4 border border-primary border-4 rounded-3">
+                        <div className="col-sm-4">
+                            <div className="d-flex justify-content-center">
+                                <div className='Affiliatedivthird'>
+                                    <img src="/Affiliateimg5.png" className="img-fluid " alt="/" />
+                                </div>
+                            </div>
+                            <br /><br />
+                        </div>
+                        <div className="col-sm-8 p-3">
+                            <p className='h2'><b>Level Up To Hall Of Fame</b></p>
+                            <br /><br />
+                            <p className='h5'>Finally, when your lifetime sales reach $50 000, you will be nominated to our Hall of Fame and receive DIAMOND status and with 50% commission.</p>
+                            <br />
+                            <p className='h5'>DIAMOND Affiliates will be invited to our Affiliate Assembly once in a year.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <br /><br /><br />
+            </div>
+
+            <div className='Affiliate-color1'>
+                <br /><br />
+                <div className="container">
+                    <p className='text-white text-center h1'><b>Our Affiliates Make Avg. $1500 In A Month!</b></p>
+                    <br />
+                    <div className="row g-0 bg-light p-4">
+                        <div className="col-sm-3 p-4">
+                            <div className="d-flex justify-content-center">
+                                <div className='Affiliatedivthird'>
+                                    <img src="/imgmen.png" className="img-fluid ronded-circle" alt="/" />
+                                </div>
+                            </div>
+                            <br />
+                            <p className='h6 text-center'><b>Life-time Commission</b></p>
+                            <p className='h6 text-center text-muted'><b>$46 742</b></p>
+                        </div>
+                        <div className="col-sm-9 p-4">
+                            <p className='h2'><b><i>"One of the best affiliate programs"</i></b></p>
+                            <br /><br />
+                            <p className='h5 text-muted'><i>"I make my living with affiliate marketing, and I am constantly looking for great products and programs. Social Media Calendar's affiliate program is a perfect match! It's super easy to sell these templates because so many need these and love them... And you get ready-made promotional materials for every product! The craziest thing is that the average commission is usually about 10-20%, but you will start with 30% with SMC!! I am fighting my way up to Diamond!"</i></p>
+                            <br />
+                            <p className='h5 text-muted'><i>- Juhani, Affiliate Marketer</i></p>
+                        </div>
+                    </div>
+                    <br />
+                    <div className="row g-0 bg-light p-4">
+                        <div className="col-sm-3 p-4">
+                            <div className="d-flex justify-content-center">
+                                <div className='Affiliatedivthird'>
+                                    <img src="/imgwomen.png" className="img-fluid ronded-circle" alt="/" />
+                                </div>
+                            </div>
+                            <br />
+                            <p className='h6 text-center'><b>Life-time Commission</b></p>
+                            <p className='h6 text-center text-muted'><b>$12 731</b></p>
+                        </div>
+                        <div className="col-sm-9 p-4">
+                            <p className='h2'><b><i>"I make $1000 extra in a month with these commissions"</i></b></p>
+                            <br /><br />
+                            <p className='h5 text-muted'><i>"I am a big fan of SMC's products by myself, and my followers ask every day how I can create such good content. It's easy to share my affiliate link with promotional code and recommend these templates and make some extra money. Now I make about $1000 in a month with SMC's commissions. It's becoming my best passive revenue stream as an influencer, and I will focus even more on this in the future.</i></p>
+                            <br />
+                            <p className='h5 text-muted'><i>- Lotta, Wellness Influencer</i></p>
+                        </div>
+                    </div>
+                </div>
+                <br />
+                <br />
+            </div>
+
+            <div className='Affiliate-color2'>
+                <br />
+                <br />
+                <br />
+                <div className="container">
+                    <div className="d-flex justify-content-center">
+                        <div className='Affiliatedivfourth'>
+                            <img src="/Affiliate3.png" className="img-fluid " alt="/" />
+                        </div>
+                    </div>
+                    <br />
+                    <p className='h1 text-center'><b>Who Is Our Affiliate Program For?</b></p>
+                    <br />
+                    <p className='h4 text-center text-muted'><b>The affiliate is suitable especially for affiliate marketers, bloggers and social media influencers. But, even if you don’t have a large followers base, you can invite your family, friends, neighbors and even strangers!</b></p>
+                    <br /><br />
+                    <p className='h1 text-center'><b>How Does Our Affiliate Program Work?</b></p>
+                    <br />
+                    <p className='h5 text-muted'><b>1. Sign in for Social Media Calendar's Affiliate Program.</b></p>
+                    <br />
+                    <p className='h5 text-muted'><b>2. Go to the affiliate program dashboard and copy your unique link for the whole web store of for a specific product.</b></p>
+                    <br />
+                    <p className='h5 text-muted'><b>3. Share the link and start earning money.</b></p>
+                    <br /><br />
+                    <div className="d-flex justify-content-center">
+                        <div className='Affiliatedivfourth'>
+                            <img src="/Affiliate4.png" className="img-fluid " alt="/" />
+                        </div>
+                    </div>
+                    <br /><br />
+                    <p className='h1 text-center'><b>How I Earn Money?</b></p>
+                    <br />
+                    <p className='h4 text-center text-muted'>
+                        <b>
+                            Every time somebody uses your link to buy our products, you will receive a 30-50% commission (depending on your tier) after 14 days. Our refund policy is 14 days, so your commission is available for payout after that.
+                            <br /><br />
+                            Your earnings will be paid with PayPal. You need to set up a PayPal account to be able to receive payments.
+                            <br /><br />
+                            You can see all your numbers when you log into your affiliate account and check the dashboard.
+                        </b>
+                    </p>
+                    <br /><br />
+                    <p className='h1 text-center'><b>How Can You Promote Our Products?</b></p>
+                    <br />
+                    <p className='h5 text-muted'>
+                        <b>
+                            Anyone can start growing a thriving affiliate business right today - thanks to the internet and social media!
+                            <br /><br />
+                            And if you already have followers, you hit the golden vein.
+                            <br /><br />
+                            Here are some examples of how you can easily make money with affiliate marketing:
+                            <br /><br />
+                            1. First, share the link with your friends, family, and contacts.
+                            <br />
+                            2. Share your affiliate link repeatedly on your social media platforms.
+                            <br />
+                            3. Share your affiliate link on your website and blog.
+                            <br />
+                            4. If you have a list, share your link with email.
+                            <br /><br />
+                            Steps 1 and 2, you can do right away. Later you can boost your sales with a website and growing an email list.
+                            <br /><br />
+                            Login to our Affiliate Partner online course to get access to tips and marketing materials.
+                        </b>
+                    </p>
+                    <br />
+                    <br />
+                    <p className='text-center h2'><b>Sign In And Start Making Money!    </b></p>
+                    <br />
+                    <div className='d-flex justify-content-center'>
+                        <Link href="/Contact">
+                            <a className="px-4">
+                                <button type="button" className="btn btn-primary px-4 py-3 btn-lg text-white"><b>REGISTER AND START SELLING</b></button>
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                <br />
+                <br />
+            </div>
+
+            <div className='frequently-asked-questions-color'>
+                <br />
+                <br />
+                <div className="">
+                    <p className='h2 text-center'><b>Frequently Asked Questions</b></p>
+                    <br /><br /><br />
+
+                    <div class="row">
+                        <div class="col-md-6 offset-md-3">
+                            <div class="accordion accordion-flush" id="faqlist">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header px-2 frequently-asked-questions-color">
+                                        <button class="accordion-button collapsed frequently-asked-questions-color" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-1">
+                                            What is my commission and when I will receive the money?
+                                        </button>
+                                    </h2>
+                                    <div id="faq-content-1" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                                        <div class="accordion-body">
+                                            We offer 30-50% commission for our Affiliates. More you sell, higher commission you will earn. Our affiliate software (Affilo) will pay your commission automatically after 14 days when the refund policy period for the order is passed.
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr className="bg-dark" />
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header px-2 frequently-asked-questions-color">
+                                        <button class="accordion-button collapsed frequently-asked-questions-color" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-2">
+                                            Do you offer shareable discount codes for affiliates?
+                                        </button>
+                                    </h2>
+                                    <div id="faq-content-2" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                                        <div class="accordion-body">
+                                            Yes we do. You can generate 10% discount code for your network in your affiliate dashboard.
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr className="bg-dark" />
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header px-2 frequently-asked-questions-color">
+                                        <button class="accordion-button collapsed frequently-asked-questions-color" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-3">
+                                            Do you help me with selling?
+                                        </button>
+                                    </h2>
+                                    <div id="faq-content-3" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                                        <div class="accordion-body">
+                                            Yes we do. We have own Facebook group for all affiliates which is a great place to ask, learn and share tips with other affiliates. You can also send is a priority message via affiliate dashboard.
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr className="bg-dark" />
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header px-2 frequently-asked-questions-color">
+                                        <button class="accordion-button collapsed frequently-asked-questions-color" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-4">
+                                            Do you offer promotional materials for your products?
+                                        </button>
+                                    </h2>
+                                    <div id="faq-content-4" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                                        <div class="accordion-body">
+                                            Yes we do. Once you finish the registration process, you will get access to "Affiliate Marketing Vault" where you will find promotional materials for almost all our porducts.
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr className="bg-dark" />
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header px-2 frequently-asked-questions-color">
+                                        <button class="accordion-button collapsed frequently-asked-questions-color" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-5">
+                                            What is MLM?
+                                        </button>
+                                    </h2>
+                                    <div id="faq-content-5" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                                        <div class="accordion-body">
+                                            MLM stands for "Multi-Level Marketing" which means that you can also invite others to become an affiliate for us. If a new affiliate sign up via your referral link, you will become a "parent affiliate" for him/her and earn 5% for every sale he/she makes.
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr className="bg-dark" />
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <br />
+                <br />
+            </div>
         </>
     );
 }
